d63430968c40ac1148d5ec7936b836c479061eaa
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-08-23 23:58:51 +0530

    Add Handler and Appropiate response for Exception

--- HotelService/src/main/java/com/example/Hotel/HotelService/exceptions/GlobalExceptionHandler.java
+++ HotelService/src/main/java/com/example/Hotel/HotelService/exceptions/GlobalExceptionHandler.java
@@ -1,13 +1,29 @@
 package com.example.Hotel.HotelService.exceptions;
 
+import com.example.Hotel.HotelService.payload.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    public ResponseEntity<Map<String,Object>> notFoundHandler(ResourceNotFoundException ex){
+//        Map map = new HashMap();
+//        map.put("message", ex.getMessage());
+//        map.put("success", false);
+//        map.put("status", HttpStatus.NOT_FOUND);
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map);
+//    }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public String resourceNotFoundExceptionHandler(ResourceNotFoundException ex){
-        return ex.getMessage();
+    public ResponseEntity<ApiResponse> handleResourceNotFoundException(ResourceNotFoundException ex){
+        String message = ex.getMessage();
+        ApiResponse response = ApiResponse.builder().message(message).success(true).status(HttpStatus.NOT_FOUND).build();
+        return new ResponseEntity<ApiResponse>(response, HttpStatus.NOT_FOUND);
     }
 }
