cff990b9444e8fb81f11dfaa30356710e85b13bc
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-08-26 02:20:28 +0530

    removing JPA Packages

--- RatingService/src/main/java/com/example/rating/RatingService/repositories/RatingRepository.java
+++ RatingService/src/main/java/com/example/rating/RatingService/repositories/RatingRepository.java
@@ -1,10 +1,8 @@
 package com.example.rating.RatingService.repositories;
 
 import com.example.rating.RatingService.entities.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.beans.JavaBean;
 import java.util.List;
 
 public interface RatingRepository extends MongoRepository<Rating, String> {
