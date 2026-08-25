bca17c442a7478a6e19ae8d809dd3420382a1654
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-08-26 00:00:36 +0530

    Initializing custom method finders

--- RatingService/src/main/java/com/example/rating/RatingService/repositories/RatingRepository.java
+++ RatingService/src/main/java/com/example/rating/RatingService/repositories/RatingRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.beans.JavaBean;
+import java.util.List;
 
 public interface RatingRepository extends MongoRepository<Rating, String> {
-
+    //custom finder methods
+    List<Rating> findByUserId(String userId);
+    List<Rating> findByHotelId(String hotelId);
 }
