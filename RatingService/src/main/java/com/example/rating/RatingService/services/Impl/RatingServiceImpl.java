82c5c41db52901e7091d07dea48a6cb5b789005e
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-08-26 02:22:37 +0530

    Adding @Service annotation

--- RatingService/src/main/java/com/example/rating/RatingService/services/Impl/RatingServiceImpl.java
+++ RatingService/src/main/java/com/example/rating/RatingService/services/Impl/RatingServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.rating.RatingService.entities.Rating;
 import com.example.rating.RatingService.repositories.RatingRepository;
 import com.example.rating.RatingService.services.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class RatingServiceImpl implements RatingService {
     @Autowired
     RatingRepository repository;
