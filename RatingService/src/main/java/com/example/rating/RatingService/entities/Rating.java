372600f2f21f420ea77a5558ec7f6acb6bf2d056
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-08-26 02:19:00 +0530

    minor changes in Package

--- RatingService/src/main/java/com/example/rating/RatingService/entities/Rating.java
+++ RatingService/src/main/java/com/example/rating/RatingService/entities/Rating.java
@@ -1,10 +1,11 @@
 package com.example.rating.RatingService.entities;
 
-import jakarta.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
