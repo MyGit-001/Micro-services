9e6e9d79387c8ae34adb478a5d4319ff8cc38c14
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-08-30 01:16:14 +0530

    adding Eureka Discovery client

--- RatingService/src/main/java/com/example/rating/RatingService/RatingServiceApplication.java
+++ RatingService/src/main/java/com/example/rating/RatingService/RatingServiceApplication.java
@@ -2,9 +2,10 @@ package com.example.rating.RatingService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class RatingServiceApplication {
 
 	public static void main(String[] args) {
