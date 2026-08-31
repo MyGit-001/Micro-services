6f629501b53435444e0d9d6ed7980af4107ac18b
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-08-31 19:05:31 +0530

    UserService Communicating with HotelService

--- UserService/src/main/java/com/example/user/UserService/services/Impl/UserServiceImpl.java
+++ UserService/src/main/java/com/example/user/UserService/services/Impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.user.UserService.services.Impl;
 
+import com.example.user.UserService.entities.Hotel;
 import com.example.user.UserService.entities.Rating;
 import com.example.user.UserService.entities.User;
 import com.example.user.UserService.exceptions.ResourceNotFoundException;
@@ -8,12 +9,15 @@ import com.example.user.UserService.services.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -46,10 +50,29 @@ public class UserServiceImpl implements UserService {
         //fetch rating of the above user from RATING-SERVICE
         //http://localhost:8083/ratings/users/1bef926f-62f7-4cd7-9119-e242092dda95
 
-        ArrayList<Rating> ratingOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
+        Rating[] ratingOfUser = restTemplate.getForObject(
+                "http://localhost:8083/ratings/users/"+user.getUserId(), Rating[].class);
         logger.info("Response from RATING-SERVICE: {}", ratingOfUser);
 
-        user.setRatings(ratingOfUser);
+        //converting Array to List
+        List<Rating> ratings = Arrays.stream(ratingOfUser).toList();
+
+        List<Rating> ratingList = ratings.stream().map(rating -> {
+            //api call to hotel service to get the hotel
+            //http://localhost:8082/hotels/bb7abba5-2378-497a-8ff4-5886c646f007
+
+            System.out.println("rating.getHotelId()");
+            ResponseEntity<Hotel> hotelResponseEntity = restTemplate.getForEntity(
+                    "http://localhost:8082/hotels/"+rating.getHotelId(), Hotel.class);
+            Hotel hotel = hotelResponseEntity.getBody();
+            logger.info("Response status code: {}", hotelResponseEntity.getStatusCode());
+
+            //set the hotel to rating
+            rating.setHotel(hotel);
+            return rating;
+        }).collect(Collectors.toList());
+
+        user.setRatings(ratingList);
         return user;
     }
 
