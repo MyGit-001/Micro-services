cef303de3720a23e0b9c247d7d04bcfb10070d95
Rishabh <prof.id.rishabhnegi@gmail.com>
2026-09-07 16:25:52 +0530

    Testing Delete Rating from UserService using Feign client

--- UserService/src/test/java/com/example/user/UserService/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/example/user/UserService/UserServiceApplicationTests.java
@@ -16,19 +16,19 @@ class UserServiceApplicationTests {
     @Autowired
     private RatingService ratingService;
 
-//    @Test
-//    void createRating() {
-//        // Test code for creating a rating
-//        Rating rating = Rating.builder()
-//                .rating(9)
-//                .userId("")
-//                .hotelId("")
-//                .feedback("Test Rating using Feign Client")
-//                .build();
-//
-//        Rating newRating = ratingService.createRating(rating);
-//        System.out.println("Created Rating: " + newRating);
-//    }
+    @Test
+    void createRating() {
+        // Test code for creating a rating
+        Rating rating = Rating.builder()
+                .rating(9)
+                .userId("user234")
+                .hotelId("hotel234")
+                .feedback("Test 2 Rating using Feign Client")
+                .build();
+
+        Rating newRating = ratingService.createRating(rating);
+        System.out.println("Created Rating: " + newRating);
+    }
 
 //    @Test
 //    void updateRating(){
@@ -42,4 +42,11 @@ class UserServiceApplicationTests {
 //        Rating updatedRating = ratingService.updateRating("6a9d5de31f741609bb49b2d1", rating);
 //        System.out.println("Updated Rating: " + updatedRating);
 //    }
+
+
+    @Test
+    void deleteRating(){
+        ratingService.deleteRating("6a9e8e7cb3c67e15b1735411");
+        System.out.println("Deleted Rating");
+    }
 }
